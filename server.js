acd4a4c2f86af790e66c0e2663d0866017dc6f79
maxine-ellah & Rahmona Henry <pair+maxine-ellah.rahmona-henry@devbootcamp.com>
2016-04-28 16:06:59 +1200

    small production config tweaks

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var config = require('./knexfile.js')
 var env = process.env.NODE_ENV || 'development'
+var port = process.env.PORT || 3000
 var knex = require('knex')(config[env])
 
 var app = require('./app')(knex)
 
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 3000! Yep! Its true!');
 });
